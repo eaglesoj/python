93c506aca5201de4b5302589ace562a8ac073d50
Justin Eagleson <jjeagleson@gmail.com>
2018-09-19 13:44:02 -0400

    initial

---
+++ test_rest.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+resp = requests.get('https://jsonplaceholder.typicode.com/todos/1', stream=True)
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET /todos/1 {}'.format(resp.status_code))
+json_data =  resp.json()
+
+# print response
+print (json_data)
+print('userId:' + str(json_data['userId']))
+print('id:' + str(json_data['id']))
+print('title:' + str(json_data['title']))
+print('completed:' + str(json_data['completed']))
